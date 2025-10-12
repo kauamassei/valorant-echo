b5fdb6422da1bc5868a95388e3a48cb222af5b8d
kauamassei <gameplayultra11@gmail.com>
2025-10-12 02:29:01 -0300

    feat(api): add get agents with axios

---
+++ src/components/Agents.tsx
@@ -0,0 +1,46 @@
+import api from "../services/api";
+import { useEffect, useState } from "react";
+
+interface Agent {
+  uuid: number;
+  displayName: string;
+  displayIcon: string;
+}
+
+const Agents = () => {
+  const [agents, setAgents] = useState<Agent[]>([]);
+
+  useEffect(() => {
+    const fetchAgents = async () => {
+      try {
+        const response = await api.get("/agents");
+        setAgents(response.data.data);
+      } catch (error) {
+        console.log("Erro na requisicao" + error);
+      }
+    };
+    fetchAgents();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <ul>
+          {agents.map((agent) => (
+            <li key={agent.uuid}>
+              <p>{agent.displayName}</p>
+              <img
+                src={agent.displayIcon}
+                alt="Imagem do Agente"
+                width={150}
+                height={150}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Agents;
