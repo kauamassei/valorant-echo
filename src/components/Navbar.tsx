90fc7740e9bf1ab83edc918ea21ce376790f4594
kauamassei <gameplayultra11@gmail.com>
2025-10-31 03:37:12 -0300

    Checkpoint

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
+import valorantEchoLogo from '../assets/valorantEchoLogo.png'
+
 const Navbar = () => {
   return (
     <header className="bg-white">
       <nav className="bg-[#14141A]">
         <div className="flex justify-between items-center h-16 p-10">
           <div>
-            <img src="" alt="Logo Valorant Echo" />
+            <img src={valorantEchoLogo} alt="Logo Valorant Echo" width={250}/>
           </div>
 
-          <ul className="flex items-center justify-between m-0 p-0 gap-20 ml-auto text-white">
+          <ul className="hidden md:flex items-center justify-between m-0 p-0 gap-20 ml-auto text-white">
             <li>
               <a href="" className="hover:text-[#FF7272] transition-all ">
                 Home
@@ -34,8 +36,10 @@ const Navbar = () => {
               </a>
             </li>
           </ul>
-
-          <button className="bg-[#FF7272] rounded-sm text-white ml-10 pl-4 pr-4 pt-1 pb-1">
+          <button className='text-1xl md:hidden bg-[#FF7272] rounded-md text-white ml-10  pl-2 pr-2 pt-1 pb-1'>
+          ☰
+          </button>
+          <button className="hidden md:flex bg-[#FF7272] rounded-sm text-white ml-10 pl-4 pr-4 pt-1 pb-1">
             Login
           </button>
         </div>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,31 @@
+import sageBackground from "../assets/sageBackground.png";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div className="relative full">
+      <Navbar />
+
+      <div className="bg-[#1c1c1c]">
+        <img
+          src={sageBackground}
+          alt="Sage Background Image"
+          className="w-full h-screen object-cover object-[80%] md:h-auto md:object-contain"
+        />
+        <div className="absolute inset-0 flex items-center justify-center">
+            
+          <div className="flex gap-4">
+            <button className="bg-[#FF7272] p-2 pl-6 pr-6 text-white border border-b-gray-200">
+              Tracker
+            </button>
+            <button className="bg-[#FF7272] p-2 pl-6 pr-6 text-white border border-b-gray-200">
+              Echo
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
