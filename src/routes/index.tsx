ff2a481d7356150361efbed1f4c38f7730ee7691
kauamassei <gameplayultra11@gmail.com>
2025-12-05 03:40:52 -0300

    refactor: profile component on profile page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import Home from "../pages/Home";
 import AgentsPage from "../pages/AgentsPage";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
-import Profile from "../components/Profile";
+import ProfilePage from "../pages/ProfilePage";
 
 
 const AppRoutes = () => {
@@ -20,7 +20,7 @@ const AppRoutes = () => {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
 
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<ProfilePage />} />
           
         </Routes>
       </>
