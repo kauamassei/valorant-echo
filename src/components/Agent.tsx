1eaa06196125a29e3ef03885ddab5d4ffa2c18d0
kauamassei <gameplayultra11@gmail.com>
2025-12-09 03:44:57 -0300

    feat: add agent abilities

--- src/components/Agent.tsx
+++ src/components/Agent.tsx
@@ -3,12 +3,19 @@ import api from "../services/api";
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
 
+interface Ability {
+  slot: string;
+  displayName: string;
+  description: string;
+  displayIcon: string;
+}
+
 interface AgenteSetup {
   uuid: string;
   displayName: string;
   description: string;
   fullPortrait: string;
-  abilities: [];
+  abilities: Ability[];
 }
 
 const Agent = () => {
@@ -46,6 +53,14 @@ const Agent = () => {
             {agent.displayName}
             {agent.description}
           </div>
+
+          {agent.abilities.map((ab) => (
+            <div className="text-white">
+              <img src={ab.displayIcon} alt="" />
+              <h3>{ab.displayName}</h3>
+              <p>{ab.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </>
