827bf28c80e8496366365f3ceec9e6c2606dee68
kauamassei <gameplayultra11@gmail.com>
2025-12-10 02:05:02 -0300

    styles: add some styles on agent page

--- src/components/Agent.tsx
+++ src/components/Agent.tsx
@@ -34,7 +34,7 @@ const Agent = () => {
     };
 
     fetchAgent();
-  }, []);
+  }, [uuid]);
 
   if (!agent) return <p>Carregando...</p>;
 
@@ -42,25 +42,30 @@ const Agent = () => {
     <>
       <Navbar />
       <div className="p-6 bg-[#070B12] min-h-screen">
-        <div className="flex">
+        <div className="flex flex-col">
+          <div className="w-180 flex items-start">
           <img
             src={agent.fullPortrait}
-            alt="Reyna full body"
-            className="w-180 flex items-start"
+            alt={agent.displayName}
           />
-
-          <div className="text-white items-center justify-center">
-            {agent.displayName}
-            {agent.description}
           </div>
 
-          {agent.abilities.map((ab) => (
-            <div className="text-white">
-              <img src={ab.displayIcon} alt="" />
-              <h3>{ab.displayName}</h3>
-              <p>{ab.description}</p>
-            </div>
-          ))}
+          <div className="text-white flex justify-end w-full">
+            <h1 className="font-['Bebas_Neue'] tracking-tight text-6xl uppercase">
+              {agent.displayName}
+            </h1>
+            <p>{agent.description}</p>
+          </div>
+
+          <div className="text-white justify-center items-center mt-8 grid grid-cols-4">
+            {agent.abilities.map((ab) => (
+              <div className="w-70 h-80 flex flex-col items-center bg-[#1e2125] p-6 gap-2 rounded-xl">
+                <img src={ab.displayIcon} alt="" width={50} />
+                <h3 className="font-semibold">{ab.displayName}</h3>
+                <p>{ab.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
